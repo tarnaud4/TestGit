26c3399302b1c06b9e0e60998e2aed57b504c974
VictorAllanic <vallanic@enssat.fr>
2022-11-03 21:27:39 +0100

    hmmm

--- Jeux/morpion.h
+++
@@ -1,4 +0,0 @@
-int victoire(int, int, char[9]); //teste toute la grille pour détecter si un alignement a lieu
-void init_grille(char[9]); //crée la grille
-void afficher_grille(char[9]); //affiche la grille actuelle
-int jouer_coup(int, char[9]); //procédure du choix de la case de jeu pour les joueurs
